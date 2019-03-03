c278a3a055974177c5df99440f06948e0dcceac5
Tyler Hueter <tylerhueter08@gmail.com>
2019-03-03 17:19:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,7 +51,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.collect do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |name, supply_array|
-    puts "  #{name.capitalize}: " + "#{supply_array}" 
+    puts "  #{name.capitalize}: " + "#{supply_array.split}" 
   
     end 
   end 
