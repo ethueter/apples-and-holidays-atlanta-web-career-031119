50fa843f2139c950f4e9276dad140ca3e828839f
Tyler Hueter <tylerhueter08@gmail.com>
2019-03-03 18:37:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ def all_holidays_with_bbq(holiday_hash)
    holiday.each do |holiday_name, supply_array|
      supply_array.each do |x|
       if x == "BBQ"
-      new_array.push("#{holiday.key}")
+      new_array.push("#{holiday.keys}")
       end
      end
    end
